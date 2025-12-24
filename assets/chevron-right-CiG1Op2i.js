5c9cfccfe200bfcf7d07cf7cab15aa05f1ae4d22
Nhat Nguyen Van (ITD) <nhat.nguyen.van@247express.vn>
2025-12-24 15:51:40 +0700

    Updates

---
+++ assets/chevron-right-CiG1Op2i.js
@@ -0,0 +1,1 @@
+import{y as o}from"./main-BCi_6bsg.js";const t=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],c=o("chevron-right",t);export{c as C};
--- assets/x-DJAyCSyZ.js
+++
@@ -1,1 +0,0 @@
-import{y as o}from"./main-DEmKiCb4.js";const e=[["path",{d:"M18 6 6 18",key:"1bl5f8"}],["path",{d:"m6 6 12 12",key:"d8bk6v"}]],c=o("x",e);export{c as X};
